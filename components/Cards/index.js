24d4d5db2dd91a71035c060a686f66ae2e5321a4
playtez <aaron.pleitez12@gmail.com>
2019-11-01 12:00:00 -0500

    MVP

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -6,6 +6,10 @@
 // This won't be as easy as just iterating over an array though.
 // Create a function that will programmatically create the following DOM component:
 //
+/////    topicArticles.map(item => {
+////   artEntry.appendChild(CardCompo(item));
+////   });
+//
 // <div class="card">
 //   <div class="headline">{Headline of article}</div>
 //   <div class="author">
@@ -17,3 +21,92 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+const artEntry = document.querySelector(".cards-container");
+
+//for (const value of topicArticles) {
+axios
+    .get("https://lambda-times-backend.herokuapp.com/articles") //
+    .then(res => {
+        res.data.articles.javascript.map(item => {
+            artEntry.appendChild(CardCompo(item));
+        });
+
+        axios
+            .get("https://lambda-times-backend.herokuapp.com/articles") //
+            .then(res => {
+                res.data.articles.bootstrap.map(item => {
+                    artEntry.appendChild(CardCompo(item));
+                });
+                axios
+                    .get("https://lambda-times-backend.herokuapp.com/articles") //
+                    .then(res => {
+                        res.data.articles.technology.map(item => {
+                            artEntry.appendChild(CardCompo(item));
+                        });
+                        axios
+                            .get("https://lambda-times-backend.herokuapp.com/articles") //
+                            .then(res => {
+                                res.data.articles.jquery.map(item => {
+                                    artEntry.appendChild(CardCompo(item));
+                                });
+                                axios
+                                    .get("https://lambda-times-backend.herokuapp.com/articles") //
+                                    .then(res => {
+                                        res.data.articles.node.map(item => {
+                                            artEntry.appendChild(CardCompo(item));
+                                        });
+                                    })
+
+                                .catch(err => {
+                                    console.log("Your code sucks this is why:", err);
+                                });
+                            })
+
+                        .catch(err => {
+                            console.log("Your code sucks this is why:", err);
+                        });
+                    })
+
+                .catch(err => {
+                    console.log("Your code sucks this is why:", err);
+                });
+            })
+
+        .catch(err => {
+            console.log("Your code sucks this is why:", err);
+        });
+    })
+
+.catch(err => {
+    console.log("Your code sucks this is why:", err);
+});
+
+function CardCompo(articles) {
+    const newCard_D = document.createElement("div");
+    newCard_D.classList.add("card");
+
+    const headLine_D = document.createElement("div");
+    headLine_D.classList.add("headline");
+    headLine_D.textContent = articles.headline;
+    newCard_D.appendChild(headLine_D);
+
+    const author_D = document.createElement("div");
+    author_D.classList.add("author");
+    // author_D.textContent = articles.authorName
+    newCard_D.appendChild(author_D);
+
+    const imgCont_D = document.createElement("div");
+    imgCont_D.classList.add("img-container");
+    // imgCont_D.textContent = articles
+    author_D.appendChild(imgCont_D);
+
+    const imgSRC_I = document.createElement("img");
+    imgSRC_I.src = articles.authorPhoto;
+    imgCont_D.appendChild(imgSRC_I);
+
+    const spanName_S = document.createElement("span");
+    spanName_S.textContent = articles.authorName;
+    author_D.appendChild(spanName_S);
+
+    return newCard_D;
+}
--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,26 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+const headCont = document.querySelector(" .header-container ");
+headCont.appendChild(Header());
+
+function Header() {
+    const header_D = document.createElement("div");
+    header_D.classList.add("header");
+
+    const date_S = document.createElement("span");
+    date_S.classList.add("date");
+    date_S.textContent = "MARCH 28, 2019";
+    header_D.appendChild(date_S);
+
+    const headline_h1 = document.createElement("h1");
+    headline_h1.textContent = " Lambda Times";
+    header_D.appendChild(headline_h1);
+
+    const temp_S = document.createElement("span");
+    temp_S.classList.add("temp");
+    temp_S.textContent = " 98°";
+    header_D.appendChild(temp_S);
+
+    return header_D;
+}
